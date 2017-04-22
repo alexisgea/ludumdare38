4c16e10341728f3a187113c3e819423fbf98d0bf
DESKTOP-EMKQ1VD\alexi <a.giard@everdreamsoft.com>
2017-04-22 12:34:21 +0200

    a sun and some more turret stuff

---
+++ PlanetProject/Assets/Scripts/Sun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+    [SerializeField] float rotationSpeed = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotation = transform.eulerAngles;
+        rotation.z += rotationSpeed;
+
+		if (rotation.z > 360) {
+            rotation.z -= 360;
+        }
+
+        transform.eulerAngles = rotation;
+
+    }
+}
