90bc5fa5c2be3320d831ad7cbeee3c96db6778eb
Sébastien de Graffenried <seb.degraff@gmail.com>
2017-04-22 22:40:54 +0200

    Dammages!

--- PlanetProject/Assets/Scripts/Destroyable.cs
+++ PlanetProject/Assets/Scripts/Destroyable.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class Destroyable : MonoBehaviour {
 
+
 	public Transform _explosionPrefab;
+
+	public float _lifePoints = 10f;
+
+	public LayerMask _dealDamagesToLayer;
+
+	public float _dealDamages = 1f;
 
 	void OnCollisionEnter2D (Collision2D collider)
 	{
-		print (collider.collider.name);
-		if (collider.gameObject.layer != LayerMask.NameToLayer ("asteroid")) {
+		// Dark magic
+		var isInLayermask = _dealDamagesToLayer == (_dealDamagesToLayer | (1 << collider.gameObject.layer));
+
+		if (isInLayermask)
+		{
+			// We want to hurt the thing we touched! -- Kamikaz
+			Destroy (gameObject);
+
+			// Damagable
+			var other = collider.gameObject.GetComponent<Destroyable> ();
+			if (other != null) {
+				Debug.LogFormat ("{0} dealing {1} damage(s) to {2}", this.name, _dealDamages, collider.gameObject.name);
+				other.Dammage (_dealDamages);
+			}
+		}
+	}
+
+	public void Dammage (float amount)
+	{
+		_lifePoints -= amount;
+
+		if (_lifePoints <= 0f) {
+			// Death
 			Destroy (gameObject);
 
 			if (_explosionPrefab != null) {
