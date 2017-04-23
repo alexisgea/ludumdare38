f3e09f1ffc78f726ad78a6c9078760543a25b062
Sébastien de Graffenried <seb.degraff@gmail.com>
2017-04-23 13:12:48 +0200

    Added zoom&sky transitions

---
+++ PlanetProject/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+	public Camera _camera;
+	public SpriteGroup _clouds;
+	public SpriteRenderer _sky;
+
+	public GameManager _gameManager;
+
+	public float _easingDuration;
+
+	public WaveParams[] _waves;
+
+	void Start ()
+	{
+		_gameManager.WaveEnd += OnWaveEnd;
+	}
+
+	void OnWaveEnd ()
+	{
+		var currentWave = _gameManager.Wave;
+		StartCoroutine (EaseToParams (_waves[currentWave]));
+	}
+
+	IEnumerator EaseToParams (WaveParams wavesParams)
+	{
+		//var zoomVelocity = 0f;;
+		var startTime = Time.time;
+
+		var startZoom = _camera.orthographicSize;
+		var targetZoom = wavesParams.cameraSize;
+
+		var startCloudColor = _clouds.color;
+		var targetCloudColor = wavesParams.cloudsColor;
+
+		var startSkyColor = _sky.color;
+		var targetSkyColor = wavesParams.skyColor;
+
+		while (true) {
+			var normalizedTime = Mathf.Clamp01 ((Time.time - startTime) / _easingDuration);
+			if (normalizedTime >= 1f) {
+				break;
+			}
+
+			// Camera zoom
+			_camera.orthographicSize = Mathf.SmoothStep (startZoom, targetZoom, normalizedTime);
+
+			// Clouds color
+			_clouds.color = ColorSmoothStep (startCloudColor, targetCloudColor, normalizedTime);
+
+			// Sky color
+			_sky.color = ColorSmoothStep (startSkyColor, targetSkyColor, normalizedTime);
+
+			yield return null;
+		}
+	}
+
+	Color ColorSmoothStep (Color start, Color end, float t)
+	{
+		return new Color (
+			Mathf.SmoothStep (start.r, end.r, t),
+			Mathf.SmoothStep (start.g, end.g, t),
+			Mathf.SmoothStep (start.b, end.b, t),
+			Mathf.SmoothStep (start.a, end.a, t)
+		);
+	}
+}
+
+[System.Serializable]
+public class WaveParams
+{
+	public float cameraSize;
+	public Color skyColor;
+	public Color cloudsColor;
+}
---
+++ PlanetProject/Assets/Scripts/SpriteGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteGroup : MonoBehaviour {
+
+	private SpriteRenderer[] _renderers;
+
+	[SerializeField]
+	private Color _color = Color.white;
+
+	// Small letter on public property to be consistent with unity apis
+	public Color color {
+		set {
+			_color = value;
+
+			foreach(var spriteRenderer in _renderers) {
+				spriteRenderer.color = _color;
+			}
+		}
+		get {
+			return _color;
+		}
+	}
+
+	void Start ()
+	{
+		_renderers = GetComponentsInChildren<SpriteRenderer> ();
+	}
+}
