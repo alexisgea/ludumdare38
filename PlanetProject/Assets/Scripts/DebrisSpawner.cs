40783676760d081cf50db0c9a1988a102c74510c
Sébastien de Graffenried <seb.degraff@gmail.com>
2017-04-23 18:01:58 +0200

    debris back + camera shake

---
+++ PlanetProject/Assets/Scripts/DebrisSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisSpawner : MonoBehaviour {
+
+	public GameObject debrisPrefab;
+	public int count = 1;
+	public float scaleRange = 0.5f;
+	public float explosionForce = 10f;
+
+	public void Spawn ()
+	{
+		for (int i = 0; i < count; i++) {
+			GameObject debris = Instantiate(debrisPrefab, transform.position, Quaternion.Euler(0,0,Random.Range(0,360)),
+				GameObject.Find("DebrisGroup").transform);
+			debris.GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle * explosionForce;
+			float scaleFactor = Random.Range(1 - scaleRange, 1 + scaleRange);
+			debris.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+		}
+	}
+}
---
+++ PlanetProject/Assets/Scripts/OrthoCameraShake.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tools
+{
+	[RequireComponent(typeof(Camera))]
+	public class OrthoCameraShake
+		: MonoBehaviour
+	{
+		public float Amplitude = 1f;
+		public float AmplitudeVariation = 0.25f;
+		public float PeekDuration = 0.1f;
+		public float EaseInDuration = 0.05f;
+		public float EaseOutDuration = 0.2f;
+
+		float _startTime;
+		Vector3 _startPosition;
+
+		[ContextMenu("Shake")]
+		public void TriggerShaking()
+		{
+			_startTime = Time.unscaledTime;
+		}
+
+		// Use this for initialization
+		void Start()
+		{
+			_startPosition = transform.localPosition;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (_startTime != 0)
+			{
+				// Compute amplitude based on elapsed time
+				float nominalAmpl = 0;
+
+				float diff = Time.unscaledTime - _startTime;
+				if (diff < EaseInDuration)
+				{
+					nominalAmpl = Mathf.Lerp(0, Amplitude, diff / EaseInDuration);
+				}
+				else
+				{
+					diff -= EaseInDuration;
+					if (diff < PeekDuration)
+					{
+						nominalAmpl = Amplitude;
+					}
+					else
+					{
+						diff -= PeekDuration;
+						if (diff < EaseOutDuration)
+						{
+							nominalAmpl = Mathf.Lerp(Amplitude, diff / EaseOutDuration, 0);
+						}
+						else
+						{
+							_startTime = 0;
+						}
+					}
+				}
+
+				// Reset to original position
+				transform.localPosition = _startPosition;
+
+				// Apply some random shaking
+				if (nominalAmpl > 0)
+				{
+					if (AmplitudeVariation > 0)
+					{
+						// Add some variation to the amplitude
+						nominalAmpl *= 1 + AmplitudeVariation * (2 * Random.value - 1);
+					}
+					var randDir = Random.insideUnitCircle.normalized;
+					transform.localPosition += (Vector3)(randDir * nominalAmpl);
+				}
+			}
+		}
+	}
+}
