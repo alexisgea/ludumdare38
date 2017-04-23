0180cd05e64dccd2380989619c489b91a8830c46
Sébastien de Graffenried <seb.degraff@gmail.com>
2017-04-23 19:29:08 +0200

    ui improvements

---
+++ PlanetProject/Assets/BuildingUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingUI : MonoBehaviour {
+
+	public Buildable _buildablePrefab;
+
+	public Text _price;
+	public CanvasGroup _group;
+
+	private GameManager _gameManager;
+
+
+	void Start ()
+	{
+		_price.text = _buildablePrefab.Cost.ToString ();
+		_gameManager = FindObjectOfType<GameManager> ();
+	}
+
+	void Update ()
+	{
+		var _canPurchase = _gameManager.Ressources >= _buildablePrefab.Cost;
+		_group.interactable = _canPurchase;
+	}
+}
